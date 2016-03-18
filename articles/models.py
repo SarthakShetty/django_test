90826b8c715a03f5a9aaf2bd592479bb23ca9ec3
Sameer Kulkarni <ksameersrk@gmail.com>
2016-03-18 16:52:44 +0530

    schema for modules

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,13 @@
+'''
+user(phone_number(pkey),age,name,photo_url,date_creation)
+group(g_id(pkey),name,date_creation, destination)
+group_message(gm_id(pkey),video_url,photo_url,text)
+user_is_admin_group(g_id(pkey)(fkey from group),phone_number(fkey from user))
+user_is_group_member(g_id(pkey)(fkey from group),phone_number(pkey)(fkey from user))
+user_send_group_message(phone_number(pkey)(fkey from user),gm_id(pkey)(fkey from group_message),g_id(fkey from group))
+user_receives_group_message(phone_number(pkey)(fkey from user),gm_id(pkey)(fkey from group_message),g_id(fkey from group))
+'''
+
 from __future__ import unicode_literals
 
 from django.db import models
